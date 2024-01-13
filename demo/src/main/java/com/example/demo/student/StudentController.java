757fd55e3205cfe6fcd1e697fe86e31a8913fbfb
LiYu <liyu.chu.work@gmail.com>
2024-01-13 20:29:11 +0800

    service layer build

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.demo.student;
 
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
-public class StudentContorller {
+public class StudentController {
     private final StudentService studentService;
-
-    public StudentContorller(StudentService studentService) {
+    @Autowired
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
